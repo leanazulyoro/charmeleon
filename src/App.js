9f9befd4251c08f7181a58fd4fe5f6dcd9fd55b4
Leandro Almeida <leanazulyoro@gmail.com>
2019-04-05 10:47:11 -0300

    Charmander & Charmeleon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import charmander from './assets/charmander.gif';
+import charmeleon from './assets/charmeleon.gif';
+
+const Charmander = () => (
+  <>
+    <h1>Charmander #004</h1>
+    <img src={charmander} />
+  </>
+)
+
+
+const Charmeleon = () => (
+  <>
+    <h1>Charmeleon #005</h1>
+    <img src={charmeleon} />
+  </>
+)
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Charmeleon />
       </div>
     );
   }
